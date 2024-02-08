248c4604241d55900b3dee9e9c49bbd8febf337e
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-08 12:16:22 +0530

    Add files via upload

---
+++ VectorAdd.java
@@ -0,0 +1,22 @@
+package adiiapplication;
+
+import java.util.*;
+public class VectorAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector list=new Vector();
+		
+		list.add("One");
+		list.add("Two");
+		list.add("Three");
+		list.add("Four");
+		
+		System.out.println(list);
+	}
+}
+
+/*
+O/P:
+[One, Two, Three, Four]
+*/
